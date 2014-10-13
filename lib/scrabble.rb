a8c467656a738fc86fd9cca3ff2f7693680e4976
David Bayendor <dbayendor@gmail.com>
2014-10-13 12:41:27 -0600

    Assessment code w/Rachel

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,5 +1,22 @@
 class Scrabble
+  LETTER_VALUES = {
+    "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+    "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+    "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+    "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+    "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+    "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+    "Y"=>4, "Z"=>10
+    }
+
   def self.score(word)
-    0
+    letters = word.to_s.upcase.strip.chars
+    return 0 if letters.empty?
+    letters.reduce(0) { |sum, letter| sum + LETTER_VALUES[letter] }
+  end
+
+  def self.highest_score_from(words)
+    high_scores = words.group_by { |word| score(word) }.max
+    high_scores[1].min_by { |word| word.length }
   end
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -1,12 +1,35 @@
 gem 'minitest'
 require './lib/scrabble'
 require 'minitest/autorun'
-require 'minitest/pride'
+# require 'minitest/pride'
 
 class ScrabbleTest < Minitest::Test
   def test_it_can_score_a_single_letter
-    skip
     assert_equal 1, Scrabble.score("a")
     assert_equal 4, Scrabble.score("f")
+  end
+
+  def test_it_can_score_word
+    assert_equal 8, Scrabble.score("hello")
+    assert_equal 4, Scrabble.score("be")
+  end
+
+  def test_it_can_score_an_uppercase_word
+    assert_equal 8, Scrabble.score("HELLO")
+    assert_equal 4, Scrabble.score("BE")
+  end
+
+  def test_it_returns_zero_for_empty_or_nil_words
+    assert_equal 0, Scrabble.score("")
+    assert_equal 0, Scrabble.score(nil)
+    assert_equal 0, Scrabble.score(' ')
+  end
+
+  def test_returns_the_highest_score_from_a_list_of_words
+    assert_equal 'home', Scrabble.highest_score_from(['word', 'home', 'hello', 'sound'])
+  end
+
+  def test_the_shortest_word_wins
+    assert_equal 'word', Scrabble.highest_score_from(['hello', 'word', 'sound'])
   end
 end
